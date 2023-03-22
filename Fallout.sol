70d982638556e22bfca504eb011377e62bc7bb37
Mohammed Rizwan <mohammedrizwann123@gmail.com>
2023-03-22 09:02:22 +0530

    Fallout added

---
+++ Fallout.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+import 'openzeppelin-contracts-06/math/SafeMath.sol';
+
+contract Fallout {
+  
+  using SafeMath for uint256;
+  mapping (address => uint) allocations;
+  address payable public owner;
+
+
+  /* constructor */
+  function Fal1out() public payable {
+    owner = msg.sender;
+    allocations[owner] = msg.value;
+  }
+
+  modifier onlyOwner {
+	        require(
+	            msg.sender == owner,
+	            "caller is not the owner"
+	        );
+	        _;
+	    }
+
+  function allocate() public payable {
+    allocations[msg.sender] = allocations[msg.sender].add(msg.value);
+  }
+
+  function sendAllocation(address payable allocator) public {
+    require(allocations[allocator] > 0);
+    allocator.transfer(allocations[allocator]);
+  }
+
+  function collectAllocations() public onlyOwner {
+    msg.sender.transfer(address(this).balance);
+  }
+
+  function allocatorBalance(address allocator) public view returns (uint) {
+    return allocations[allocator];
+  }
+}
+
+
+// Vulnerabilities
+// In older version of solidity, the contract name was used to create the constructor but the constructor name had to be exactly same as contract name. Here in smart contract if differs which can be easily hacked and take the ownership
+
+// Recommended Mitigation steps
+// In newer solidity versions, constructor as separate function is introduced. In older versions using this kind of contract architecture, make sure the contract name and constructor name are exactly equal. 
